0a2f9b36c6eb34818cfbe6f8730fcb1a021e9b7a
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-06-10 23:02:26 +0100

    Profile component

---
+++ src/components/Profile.js
@@ -0,0 +1,18 @@
+import "./Profile.css";
+import image from "../logo.webp"
+
+export default function Profile() {
+  return (
+    <div className="profile">
+      <div className="info">
+        <img src={image} />
+        <h2>Jack Pettigrew</h2>
+      </div>
+      <div className="subtitle">
+        <h3>Gamer.</h3>
+        <h3>Game Developer.</h3>
+        <h3>Nerd.</h3>
+      </div>
+    </div>
+  );
+}
