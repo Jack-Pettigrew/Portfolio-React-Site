ec4b254875be05c6fc6596771d27f220b20ea6cb
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-06-12 21:45:40 +0100

    Font + global css

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
 import Profile from './components/Profile';
+import FeaturedProjectViewer from './components/FeaturedProjectViewer';
 
 // styles
 import "./global.css"
+import Links from './components/Links';
 
 function App() {
   return (
     <div className="App">
       <Profile />
-
+      <Links />
+      <FeaturedProjectViewer />
+      Univeristy/Personal/Polygonal project selectors
     </div>
   );
 }
