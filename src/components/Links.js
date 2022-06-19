651852e7aaea07a73267bd429c5b2905aae98f40
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-06-19 11:32:41 +0100

    noreferrer vulnerability fix

--- src/components/Links.js
+++ src/components/Links.js
@@ -8,8 +8,8 @@ export default function Links({transitionClass}) {
   return (
       <div className="links">
         <div className="links-container">
-            <a href="https://github.com/Jack-Pettigrew" target="_blank"><FaGithub className={"icon " + transitionClass} /></a>
-            <a href="https://pixeldump.itch.io/" target="_blank"><FaItchIo className={"icon " + transitionClass} /></a>
+            <a href="https://github.com/Jack-Pettigrew" target="_blank" rel="noopener noreferrer"><FaGithub className={"icon " + transitionClass} /></a>
+            <a href="https://pixeldump.itch.io/" target="_blank" rel="noopener noreferrer"><FaItchIo className={"icon " + transitionClass} /></a>
         </div>
     </div>
   )
