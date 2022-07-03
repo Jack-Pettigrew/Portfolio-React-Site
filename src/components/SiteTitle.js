07c48fbb863176a35aa48633c76ab9f07c8466ff
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-07-03 10:52:09 +0100

    Added subtitle

--- src/components/SiteTitle.js
+++ src/components/SiteTitle.js
@@ -1,40 +1,43 @@
-import "./SiteTitle.css"
+import "./SiteTitle.css";
 
 import { useEffect } from "react";
-import Links from "./Links"
+import Links from "./Links";
 
 export default function SiteTitle() {
-    
-    // Set position to be normal - CSS doesn't animate it right
-    setTimeout(() => {
-        let title = document.querySelector('.title');
-        title.classList.remove('overlayed')
-    }, 4000);
+  // Set position to be normal - CSS doesn't animate it right
+  setTimeout(() => {
+    let title = document.querySelector(".title");
+    title.classList.remove("overlayed");
+  }, 4000);
 
-    // Rotate link one by one
-    useEffect(() => {
-        const flipLink = async () => {
-            const list = document.querySelectorAll('.link');
+  // Rotate link one by one
+  useEffect(() => {
+    const flipLink = async () => {
+      const list = document.querySelectorAll(".link");
 
-            for (let i = 0; i < list.length; i++) {
-                await new Promise((resolve) => {
-                    setTimeout(function () {
-                        list[i].classList.add('visible-rotated');
-                        resolve();
-                    }, 250)
-                });
+      for (let i = 0; i < list.length; i++) {
+        await new Promise((resolve) => {
+          setTimeout(function () {
+            list[i].classList.add("visible-rotated");
+            resolve();
+          }, 250);
+        });
+      }
+    };
 
-            }
-        };
+    // Delay call
+    setTimeout(flipLink, 4000);
+  });
 
-        // Delay call
-        setTimeout(flipLink, 4000);
-    });
-    
-    return (
-    <div className='title overlayed'>
-        <p className="name">Jack Pettigrew</p>
-        <Links introTransitionClass="hidden-rotated" />
+  return (
+    <div className="title overlayed">
+      <p className="name">Jack Pettigrew</p>
+      <div className="subtitle">
+        <h3>Gamer.</h3>
+        <h3>Game Developer.</h3>
+        <h3>Nerd.</h3>
+      </div>
+      <Links introTransitionClass="hidden-rotated" />
     </div>
-)
+  );
 }
