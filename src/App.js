b5d470a7fd2f09df1e45bb6b3542e5a4a083d3dc
Jack-Pettigrew <jak.pettigrew@gmail.com>
2022-10-04 19:53:47 +0100

    Moved Home component into it's own file

--- src/App.js
+++ src/App.js
@@ -3,45 +3,18 @@ import "./global.css";
 import "./App.css";
 
 // components
-import SiteTitle from "./components/SiteTitle";
-import Transition from "./components/Transition";
-import FeaturedProjectsViewer from "./components/FeaturedProjectsViewer";
-import WhatImWorkingOn from "./components/WhatImWorkingOn";
-import Category from "./components/Category";
-import CategoryList from "./components/CategoryList";
-import GameCharacterIcon from "./components/GameCharacterIcon";
-import NavMenu from "./components/NavMenu";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-const Home = function () {
-  return (
-    <div className="App">
-      <SiteTitle />
-      <Transition />
-      <NavMenu />
-      <FeaturedProjectsViewer />
-      <WhatImWorkingOn />
-      <CategoryList>
-        <Category catgoryName={"Studied"} />
-        <Category catgoryName={"Explored"} />
-        <Category catgoryName={"Polygonal"} />
-      </CategoryList>
-      <GameCharacterIcon
-        characterSrc="/images/characters/lilal.png"
-        width="75px"
-        height="75px"
-      />
-    </div>
-  );
-};
-
-// Move pages into their own files
+// pages
+import Home from "./pages/Home";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={Home()}></Route>
+        <Route path="/" element={<Home />} />
+        {/* <Route path="/projects/game-dev" element={<Projects />} /> */}
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/Home.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import SiteTitle from "../components/SiteTitle";
+import Transition from "../components/Transition";
+import FeaturedProjectsViewer from "../components/FeaturedProjectsViewer";
+import WhatImWorkingOn from "../components/WhatImWorkingOn";
+import Category from "../components/Category";
+import CategoryList from "../components/CategoryList";
+import GameCharacterIcon from "../components/GameCharacterIcon";
+import NavMenu from "../components/NavMenu";
+
+export default function Home() {
+  return (
+    <div className="App">
+      <SiteTitle />
+      <Transition />
+      <NavMenu />
+      <FeaturedProjectsViewer />
+      <WhatImWorkingOn />
+      <CategoryList>
+        <Category catgoryName={"Studied"} />
+        <Category catgoryName={"Explored"} />
+        <Category catgoryName={"Polygonal"} />
+      </CategoryList>
+      <GameCharacterIcon
+        characterSrc="/images/characters/lilal.png"
+        width="75px"
+        height="75px"
+      />
+    </div>
+  );
+}
