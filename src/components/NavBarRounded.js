73bb467bdf49781625d9720cbcedc4ef30304949
Jack-Pettigrew <jak.pettigrew@gmail.com>
2023-01-11 21:38:37 +0000

    Home screen social links + min height

--- src/components/NavBarRounded.js
+++ src/components/NavBarRounded.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 export default function NavBarRounded() {
   return (
     <div className="flex w-full justify-center sm:justify-end">
-      <div className="flex justify-center items-center gap-8 text-[1.3rem] text-center m-3 px-5 py-2 bg-[rgba(38,38,38,0.75)] rounded-xl backdrop-blur-md">
+      <div className="flex w-[90%] sm:w-fit justify-center items-center gap-8 text-[1.3em] text-center m-3 px-5 py-2 bg-[rgba(38,38,38,0.75)] rounded-xl backdrop-blur-md">
           <Link className="duration-200 hover:text-[#a23fff] hover:-translate-y-[3px] hover:text font-bold" to="/"><h1>Home</h1></Link>
           <Link className="duration-200 hover:text-[#a23fff] hover:-translate-y-[3px] hover:text font-bold" to="/about-me"><h1>About Me</h1></Link>
           <Link className="duration-200 hover:text-[#a23fff] hover:-translate-y-[3px] hover:text font-bold" to="/projects/game-dev"><h1>Game Dev</h1></Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ import FooterRounded from "../components/FooterRounded";
 import GameCharacterIcon from "../components/GameCharacterIcon";
 import NavBar from "../components/NavBar";
 import NavBarRounded from "../components/NavBarRounded";
+import SocialLinks from "../components/SocialLinks";
 
 export default function Home() {
   return (
@@ -14,21 +15,23 @@ export default function Home() {
       >
       </div> */}
 
-      <div className="flex flex-col w-full h-full sm:h-screen text-white items-center justify-between bg-center bg-cover" style={{ backgroundImage: "url(images/pcgcity.webp)" }}>
+      <div className="flex flex-col w-screen h-full min-h-screen text-white items-center justify-between bg-center bg-cover" style={{ backgroundImage: "url(images/pcgcity.webp)" }}>
         {/* <NavBar /> */}
         <NavBarRounded />
 
-        <div className="flex flex-col justify-center items-center w-[90vw] sm:w-[70vw] sm:my-3 py-8 rounded-[40px] backdrop-blur-md bg-[rgba(38,38,38,0.75)] shadow-[5px_5px_20px_0px_rgba(0,0,0,0.5)]">
+        <div className="flex flex-col justify-center items-center w-[90vw] sm:w-[60vw] sm:my-3 py-8 rounded-[40px] backdrop-blur-md bg-[rgba(38,38,38,0.75)] shadow-[5px_5px_20px_0px_rgba(0,0,0,0.5)]">
           {/* <a id="toggleInfoBtn" className="absolute top-5 right-7">Test</a> */}
           
           <img src="/logo_225.webp" width="150px" height="150px" className="rounded-full drop-shadow-lg" alt="Profile Picture" />
           <h1 className="text-center text-5xl p-5">Jack Pettigrew</h1>
-          <div id="headline-info" className="flex flex-col gap-5 px-5 text-center w-full text-xl font-bold">
+          <div id="headline-info" className="flex flex-col gap-5 px-5 py-5 text-center w-full text-xl font-bold">
             <p>A Software Developer and First-Class Games Computing graduate with a passion for Video Games and a burning desire to contribute fantastical games and ideas to the Games Industry.</p>
             <p>University has taught me many lessons (and not just those on the syllabus). It has been the foundation in learning necessary technical skills, forming a healthy and enthusiastic mindset, as well as providing the next step towards what I aspire to be.</p>
           </div>
             
-            <span className="pt-10"><GameCharacterIcon characterSrc="images/characters/lilal.webp" /></span>
+          <GameCharacterIcon characterSrc="images/characters/lilal.webp" width="75px" height="75px" />
+
+          <span className="mt-5 -mb-5"><SocialLinks /></span>
         </div>
 
         {/* <Footer /> */}
