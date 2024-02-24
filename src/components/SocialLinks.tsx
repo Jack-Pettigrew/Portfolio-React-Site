ae9e12c29ebf8bb2a85c920afc346806248462d3
Jack Pettigrew <jak.pettigrew@gmail.com>
2024-02-24 22:42:54 +0000

    Removed commented line

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -11,7 +11,6 @@ export default function SocialLinks({ linksFlexDirection = "row" }) {
       <a className={className} href="https://pixeldump.itch.io/" target="_blank" rel="noopener noreferrer"><FaItchIo /></a>
       <a className={className} href="https://youtube.com/@DarkDax" target="_blank" rel="noopener noreferrer"><FaYoutube /></a>
       <a className={className} href="https://www.linkedin.com/in/jack-pettigrew/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-      {/* <a className={`text-white bg-[#232323] m-2 p-1 rounded-[10px] duration-200 hover:scale-[1.2] drop-shadow-md`} href="https://twitter.com/ThisIsDarkDax" target="_blank" rel="noopener noreferrer"><FaTwitter /></a> */}
     </div>
   )
 }
