6de725a0106798707dd9142b0af3458d29936f10
Jack Pettigrew <jak.pettigrew@gmail.com>
2024-03-12 21:56:50 +0000

    Projects page styling fix

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -7,12 +7,12 @@ export default function projects() {
     const projects = projectsFile.projects;
 
     return (
-        <div className="flex flex-col">
-            <div className="lg:sticky lg:top-2 lg:z-10 bg-white/50 w-fit rounded-lg backdrop-blur-md mt-3 mx-5 p-1">
+        <div className="flex flex-col items-center">
+            <div className="lg:sticky lg:top-2 lg:z-10 bg-white/50 w-full rounded-lg backdrop-blur-md mt-3 mx-5 p-1 pl-5">
                 <Breadcrumb />
             </div>
 
-            <div className="py-2 px-5 grid gap-5 grid-cols-1 lg:grid-cols-3">
+            <div className="py-2 px-5 grid gap-5 grid-cols-1 md:grid-cols-3 xl:gap-6 xl:grid-cols-4">
                 {projects.map(function (project, index) {
                     return (
                         <Link key={index} href={"/projects/" + project.id}>
