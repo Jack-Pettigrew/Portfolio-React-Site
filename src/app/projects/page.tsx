f6d0d2de33fcc2478bbc36c51c6111ed0b491e8f
Jack Pettigrew <jak.pettigrew@gmail.com>
2024-03-19 18:21:05 +0000

    Fixed projects page breadcrumb + card changes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -8,21 +8,23 @@ export default function projects() {
 
     return (
         <div className="flex flex-col items-center">
-            <div className="lg:sticky lg:top-2 lg:z-10 bg-white/50 w-full rounded-lg backdrop-blur-md mt-3 mx-5 p-1 pl-5">
-                <Breadcrumb />
-            </div>
+            <div className="flex flex-col gap-1 py-2 px-5">
+                <div className="lg:sticky lg:top-2 lg:z-10 bg-white/50 dark:bg-slate-500/25 rounded-lg backdrop-blur-md mt-3 mb-1 lg:mt-0 lg:mb-3 px-3 py-2 w-fit">
+                    <Breadcrumb />
+                </div>
 
-            <div className="py-2 px-5 grid gap-5 grid-cols-1 md:grid-cols-3 xl:gap-6 xl:grid-cols-4">
-                {projects.map(function (project, index) {
-                    return (
-                        <Link key={index} href={"/projects/" + project.id}>
-                            <ImageHeaderCard imageSrc={project.headerImage}>
-                                <h1>{project.title}</h1>
-                                <p>{project.tagline}</p>
-                            </ImageHeaderCard>
-                        </Link>
-                    )
-                })}
+                <div className="grid gap-5 grid-cols-1 md:grid-cols-3 xl:gap-6 xl:grid-cols-4">
+                    {projects.map(function (project, index) {
+                        return (
+                            <Link key={index} href={"/projects/" + project.id}>
+                                <ImageHeaderCard imageSrc={project.headerImage ?? "/images/characters/lilal.webp"}>
+                                    <h1>{project.title}</h1>
+                                    <p>{project.tagline}</p>
+                                </ImageHeaderCard>
+                            </Link>
+                        )
+                    })}
+                </div>
             </div>
         </div>
 
