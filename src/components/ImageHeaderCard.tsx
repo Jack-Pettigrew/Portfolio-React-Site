9688da63af405452b2e55743b6cab5910d13a2ae
Jack Pettigrew <jak.pettigrew@gmail.com>
2024-03-23 18:56:25 +0000

    Improved ImageHeaderCard

--- src/components/ImageHeaderCard.tsx
+++ src/components/ImageHeaderCard.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export default function ImageHeaderCard({ children, imageSrc, cardHeaderIcon, className }: { children?: ReactNode, imageSrc: string, cardHeaderIcon?: string, className?:string }) {
@@ -5,7 +6,10 @@ export default function ImageHeaderCard({ children, imageSrc, cardHeaderIcon, cl
     return (
         <div className={className + " group flex flex-col basis-full lg:max-w-80 rounded-xl overflow-hidden shadow-default"}>
             <div className="relative text-pretty">
-                <img src={imageSrc} className={"w-full aspect-[6/2] object-cover object-center"} />
+                <div id="image-header-container" className="relative h-28">
+                    <Image alt="Project Headr Image" fill src={imageSrc} className={"w-full aspect-[6/2] object-cover object-center"} />
+                </div>
+                
                 {cardHeaderIcon &&
                     <p className="absolute duration-200 text-center w-[64px] h-[64px] top-[calc(100%-32px)] left-[calc(50%-32px)] group-hover:top-[calc(90%-32px)] group-hover:shadow-default bg-white rounded-full p-3 text-3xl border-2">{cardHeaderIcon}</p>
                 }
